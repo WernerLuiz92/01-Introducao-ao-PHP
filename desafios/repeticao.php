7b7500d5f7ed83332e45ddd88d963c739ea43756
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-23 00:17:22 -0300

    Estruturas de repeticao FOR e WHILE

---
+++ desafios/repeticao.php
@@ -0,0 +1,12 @@
+<?php
+
+    for ($i = 1; $i <= 15; $i++) {
+        echo "#$i: FOR".PHP_EOL;
+    }
+
+    $i=1;
+
+    while ($i <= 15) {
+        echo "#$i: WHILE".PHP_EOL;
+        $i++;
+    }
