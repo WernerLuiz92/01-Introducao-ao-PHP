2277cb05658ac5a385575aa3077742c515eca1d3
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-23 00:02:02 -0300

    Estrutura de decisao IF

--- estruturasDeDecisao.php
+++ estruturasDeDecisao.php
@@ -7,7 +7,7 @@
 
     if ($idade >= 18) {
         echo "Você tem $idade anos. Pode entrar sozinho.";
-    } else if ($idade >= 16 && $numeroDePessoas > 1){
+    } elseif ($idade >= 16 && $numeroDePessoas > 1){
         echo "Você tem $idade anos e está acompanhado(a), então pode entrar.";
     } else {
         echo "Você só tem $idade anos. Você não pode entrar.";
